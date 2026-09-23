f3d858628e473f5ce5f4b7922ee76371bcb7eaab
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2026-09-23 09:05:00 -0300

    A

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "src/lista.h"
+#include <stdbool.h>
 
 const int ERROR = -1;
 const int CANTIDAD_MINIMA_PARAMETROS = 3;
 const char RESULTADO_INVALIDO = 'E';
 
-bool iteradores_pueden_iterar(lista_iterador_t **iteradores, size_t tope) {
-	for(size_t i = 0; i < tope; i++) {
+bool iteradores_pueden_iterar(lista_iterador_t **iteradores, int tope) {
+	for(int i = 0; i < tope; i++) {
 		if(!lista_iterador_se_puede_iterar(iteradores[i])) {
 			return false;
 		}
@@ -42,6 +43,33 @@ lista_t *parsear_lista(char* texto) {
 	return lista;
 }
 
+void liberar_memoria_iteradores(lista_iterador_t **iteradores, int tope) {
+	for(int j = 0; j < tope; j++) {
+		lista_iterador_destruir(iteradores[j]);
+	}
+}
+
+void liberar_memoria_listas(lista_t **listas, int tope) {
+	for(int l = 0; l < tope; l++) {
+		lista_destruir_todo(listas[l], free);
+	}
+}
+
+bool operar(char operador, void *operandos, int tope, int *resultado) {
+	if(operador == '+') {
+
+	} else if(operador == '-') {
+
+	} else if(operador == '*') {
+
+	} else if(operador == '/') {
+
+	} 
+
+	return resultado;
+
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < CANTIDAD_MINIMA_PARAMETROS) {
@@ -53,45 +81,64 @@ int main(int argc, char **argv)
 
 	int cantidad_listas = argc - 2;
 	lista_t **listas = malloc(sizeof(lista_t *) * cantidad_listas);
-	if(!listas) return NULL;
+	if(!listas) return ERROR;
 
 	for(size_t i = 0; i < argc; i++) {
 		listas[i] = parsear_lista(argv[i+2]);
 	}
 
 	lista_iterador_t **iteradores = malloc(sizeof(lista_iterador_t *) * cantidad_listas);
+	if(!iteradores) {
+		liberar_memoria_listas(listas, cantidad_listas);
+		return ERROR;
+	}
 
 	for(int i = 0; i < cantidad_listas; i++) {
 		iteradores[i] = lista_iterador_crear(listas[i]);
 	}
 
 	lista_t *resultados = lista_crear();
-	if(!resultados) return ERROR;
+	if(!resultados)  {
+		liberar_memoria_listas(listas, cantidad_listas);
+		liberar_memoria_iteradores(iteradores, cantidad_listas);
+		return ERROR;
+	}
 
 	while(iteradores_pueden_iterar(iteradores, cantidad_listas)) {
+		int *elementos = malloc(sizeof(int) * cantidad_listas);
+		if(!elementos) {
+			liberar_memoria_listas(listas, cantidad_listas);
+			liberar_memoria_iteradores(iteradores, cantidad_listas);
+			return ERROR;
+		}
+		int tope_elementos = 0;
 
 		for (size_t i = 0; i < cantidad_listas; i++) {
-			void *elemento =
-				lista_iterador_obtener_elemento(iteradores[i]);
+			elementos[i] = lista_iterador_obtener_elemento(iteradores[i]);
+			tope_elementos++;
+		}
 
-			// usar elemento
+		size_t cant_actual = lista_cantidad(resultados);
+		int resultado = 0;
+		bool operacion_valida = operar(argv[1], elementos, tope_elementos, &resultado); 
+
+		if(operacion_valida) {
+			lista_insertar(resultados, resultado, cant_actual);
+		} else {
+			lista_insertar(resultados, 'E', cant_actual);
 		}
 
 		for (size_t i = 0; i < cantidad_listas; i++) {
 			lista_iterador_siguiente(iteradores[i]);
 		}
+
+		free(elementos);
 	}
 
 	imprimir_resultados(resultados);
-
-
 	
-	for(size_t j = 0; j < cantidad_listas; j++) {
-		lista_iterador_destruir(iteradores[j]);
-	}
-	for(size_t l = 0; l < cantidad_listas; l++) {
-		lista_destruir_todo(listas[l], free);
-	}
+	liberar_iteradores(iteradores, cantidad_listas);
+	liberar_memoria_listas(listas, cantidad_listas);
 	lista_destruir(resultados);
 
 	return 0;
